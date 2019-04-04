e6521cd37f877e1b79828c40e8a3b891876542f8
于栋安 <769555955@qq.com>
2019-04-04 09:08:35 +0800

    Add files via upload

---
+++ WebsInfo.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+'''
+@author: Doran
+@license: www.ibud.top
+@contact: yudongan97@gmail.com
+@software: pycharm
+@file: WebsInfo.py
+@time: 2019/4/3 16:09
+@desc:
+'''
+
+
+class WebsInfo:
+    def __init__(self, urlRes = None, urlHeader = None, urlType = None):
+        self.urlRes = urlRes
+        self.urlHeader = urlHeader
+        self.urlType = urlType
+
+
+if __name__ == '__main__':
+    print("hello")
